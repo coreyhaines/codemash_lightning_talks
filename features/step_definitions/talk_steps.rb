6bf3dbe461053afc39e0a8c85e9829d68621bd18
coreyhaines <coreyhaines@gmail.com>
2012-01-07 17:23:02 -0800

    Steps defined for submitting a proposal

--- features/step_definitions/talk_steps.rb
+++ features/step_definitions/talk_steps.rb
@@ -1,12 +1,18 @@
 Given /^I'm on the talk proposal page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit proposals_url
 end
 
 When /^I submit my proposal$/ do
-  pending # express the regexp above with the code you wish you had
+  fill_in "Name", :with => "Corey"
+  fill_in "Email Address", :with => "coreyhaines@example.com"
+  fill_in "Twitter Handle", :with => "@coreyhaines"
+  fill_in "Topic", :with => "Very interesting talk"
+  click_button "Submit my lightning talk"
 end
 
 Then /^I should see it on the list$/ do
-  pending # express the regexp above with the code you wish you had
+  within(".proposals") do
+    page.should have_css(".proposal", :text => "Very interesting talk")
+  end
 end
 
