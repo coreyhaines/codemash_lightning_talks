db5e74c888ced3f95a41bc34e0e1cfe87417df10
coreyhaines <coreyhaines@gmail.com>
2012-01-07 17:04:29 -0800

    Set up application structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CodemashLightningTalks::Application.config.secret_token = 'f9bc52ea770aaf8bf556d9d43db48dc21dcd91db3583523c6c89866a153aad6227493096078f194938372ff30050d73331985f9e58a4587b6999eb036f484276'
