c3fb1d25c2c539cda7008c75db21cb6249082ec5
coreyhaines <coreyhaines@gmail.com>
2012-01-10 06:52:54 -0800

    Show message for no twitter handle

--- app/helpers/proposals_helper.rb
+++ app/helpers/proposals_helper.rb
@@ -1,6 +1,7 @@
 module ProposalsHelper
   def twitter_handle_as_link(handle)
+    return "no twitter handle" if handle.blank?
     handle = handle[1..-1] if handle.start_with?("@")
-    link_to handle, "http://twitter.com/#{handle}"
+    link_to "@#{handle}", "http://twitter.com/#{handle}"
   end
 end
